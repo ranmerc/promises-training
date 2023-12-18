dc43893638f2503b9199eedb6ded0fb0d0877d3c
Kamran Ansari <ranmerc@outlook.com>
2023-12-18 09:38:26 +0530

    graph/2: completed

--- src/exercises/graph/2/exercise.ts
+++ src/exercises/graph/2/exercise.ts
@@ -7,14 +7,38 @@ const mixed =
 
 const asyncAwait =
   ({ createPromise }: ExerciseContext) =>
-  async () => {};
+  async () => {
+    const firstChain = async () => {
+      await createPromise("A");
+      await createPromise("B");
+      await createPromise("C");
+    };
+
+    const secondChain = async () => {
+      await createPromise("D");
+      await createPromise("E");
+      await createPromise("F");
+    };
+
+    await Promise.all([firstChain(), secondChain()]);
+  };
 
 const thenCatch =
   ({ createPromise }: ExerciseContext) =>
-  async () => {};
+  async () => {
+    const firstChain = createPromise("A")
+      .then(() => createPromise("B"))
+      .then(() => createPromise("C"));
+
+    const secondChain = createPromise("D")
+      .then(() => createPromise("E"))
+      .then(() => createPromise("F"));
+
+    return Promise.all([firstChain, secondChain]);
+  };
 
 export default {
   makeMixedExercise: skipExercise(mixed),
-  makeAsyncAwaitExercise: skipExercise(asyncAwait),
-  makeThenCatchExercise: skipExercise(thenCatch),
+  makeAsyncAwaitExercise: asyncAwait,
+  makeThenCatchExercise: thenCatch,
 };
