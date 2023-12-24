f5dc3f0ff70e12c22f35b0ce45c4a89332df08df
Kamran Ansari <ranmerc@outlook.com>
2023-12-24 23:29:05 +0530

    graph/8: completed

--- src/exercises/graph/8/exercise.ts
+++ src/exercises/graph/8/exercise.ts
@@ -7,14 +7,42 @@ const mixed =
 
 const asyncAwait =
   ({ createPromise }: ExerciseContext) =>
-  async () => {};
+  async () => {
+    const firstChain = async () => {
+      await createPromise("A");
+
+      await Promise.all([createPromise("B"), createPromise("C")]);
+
+      await createPromise("D");
+    };
+
+    const secondChain = async () => {
+      await createPromise("E");
+
+      await Promise.all([createPromise("F"), createPromise("G")]);
+
+      await createPromise("H");
+    };
+
+    await Promise.all([firstChain(), secondChain()]);
+  };
 
 const thenCatch =
   ({ createPromise }: ExerciseContext) =>
-  async () => {};
+  async () => {
+    const firstChain = createPromise("A")
+      .then(() => Promise.all([createPromise("B"), createPromise("C")]))
+      .then(() => createPromise("D"));
+
+    const secondChain = createPromise("E")
+      .then(() => Promise.all([createPromise("F"), createPromise("G")]))
+      .then(() => createPromise("H"));
+
+    await Promise.all([firstChain, secondChain]);
+  };
 
 export default {
   makeMixedExercise: skipExercise(mixed),
-  makeAsyncAwaitExercise: skipExercise(asyncAwait),
-  makeThenCatchExercise: skipExercise(thenCatch),
+  makeAsyncAwaitExercise: asyncAwait,
+  makeThenCatchExercise: thenCatch,
 };
