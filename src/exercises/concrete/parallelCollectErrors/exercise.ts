2f064cdabc1b2ad7006564914bd4a44d74fa722a
Kamran Ansari <ranmerc@outlook.com>
2023-12-25 18:14:31 +0530

    concrete/parallelCollectErrors: completed

--- src/exercises/concrete/parallelCollectErrors/exercise.ts
+++ src/exercises/concrete/parallelCollectErrors/exercise.ts
@@ -3,4 +3,50 @@ type Context = {
 };
 
 export default ({ postData }: Context) =>
-  async (list: Array<string>) => {};
+  async (list: Array<string>) => {
+    const results = await Promise.allSettled(
+      list.map(async (data) => await postData(data))
+    );
+
+    // reduce to expected resultant object directly
+    // return results.reduce(
+    //   (returnObject, result) => {
+    //     if (result.status === "fulfilled") {
+    //       return {
+    //         ...returnObject,
+    //         successes: returnObject.successes.concat(result.value),
+    //       };
+    //     } else {
+    //       return {
+    //         ...returnObject,
+    //         errors: returnObject.errors.concat(result.reason),
+    //       };
+    //     }
+    //   },
+    //   {
+    //     successes: [] as string[],
+    //     errors: [] as string[],
+    //   }
+    // );
+
+    const successes = results.reduce((successResults, result) => {
+      if (result.status === "fulfilled") {
+        return successResults.concat(result.value);
+      }
+
+      return successResults;
+    }, [] as string[]);
+
+    const errors = results.reduce((errorResults, result) => {
+      if (result.status === "rejected") {
+        return errorResults.concat(result.reason);
+      }
+
+      return errorResults;
+    }, [] as string[]);
+
+    return {
+      successes,
+      errors,
+    };
+  };
