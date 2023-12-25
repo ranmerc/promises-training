dddf51d985ed3be63fa57074ca1369f4d2209895
Kamran Ansari <ranmerc@outlook.com>
2023-12-25 14:58:10 +0530

    concrete/serialCollectErrors: completed

--- src/exercises/concrete/serialCollectErrors/exercise.ts
+++ src/exercises/concrete/serialCollectErrors/exercise.ts
@@ -3,4 +3,22 @@ type Context = {
 };
 
 export default ({ postData }: Context) =>
-  async (list: Array<string>) => {};
+  async (list: Array<string>) => {
+    const successes: string[] = [];
+    const errors: unknown[] = [];
+
+    for (const data of list) {
+      try {
+        const success = await postData(data);
+
+        successes.push(success);
+      } catch (e) {
+        errors.push(e);
+      }
+    }
+
+    return {
+      successes,
+      errors,
+    };
+  };
