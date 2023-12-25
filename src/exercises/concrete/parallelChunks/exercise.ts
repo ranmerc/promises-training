22ccfa411a12079e8c06d49b5e248f42bd55b085
Kamran Ansari <ranmerc@outlook.com>
2023-12-25 19:25:53 +0530

    concrete/parallelChunks: Beginner level completed

--- src/exercises/concrete/parallelChunks/exercise.ts
+++ src/exercises/concrete/parallelChunks/exercise.ts
@@ -5,4 +5,25 @@ type Context = {
 };
 
 export default ({ postData }: Context) =>
-  async (list: Array<string>) => {};
+  async (list: Array<string>) => {
+    const CHUNK_SIZE = 5;
+    const results: string[] = [];
+
+    let currentIndex = 0;
+    while (currentIndex < list.length) {
+      const endIndex = currentIndex + CHUNK_SIZE;
+
+      // endIndex is not included in slice
+      const listBatch = list.slice(currentIndex, endIndex);
+
+      const batchResults = await Promise.all(
+        listBatch.map(async (data) => await postData(data))
+      );
+
+      results.push(...batchResults);
+
+      currentIndex = endIndex;
+    }
+
+    return results;
+  };
