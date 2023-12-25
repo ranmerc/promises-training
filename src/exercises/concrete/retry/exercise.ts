a8d983cefb2329b583fd85be7181538a1ddbb3c3
Kamran Ansari <ranmerc@outlook.com>
2023-12-25 13:53:14 +0530

    concrete/retry: completed

--- src/exercises/concrete/retry/exercise.ts
+++ src/exercises/concrete/retry/exercise.ts
@@ -3,4 +3,22 @@ type Context = {
 };
 
 export default ({ getData }: Context) =>
-  async (data: string) => {};
+  async (data: string) => {
+    const MAX_RETRIES = 3;
+    let retryCount = 0;
+    let errors: unknown[] = [];
+
+    while (retryCount <= MAX_RETRIES) {
+      try {
+        const result = await getData(data);
+
+        return result;
+      } catch (e) {
+        errors = errors.concat(e);
+
+        retryCount++;
+      }
+    }
+
+    throw errors;
+  };
